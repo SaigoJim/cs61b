763e1438d15702d15d1c06c5c11fa957276b27a5
SaigoJim <1533137229@qq.com>
2021-04-30 16:45:07 +0800

    modified equalChars

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,8 +1,12 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-        x = Character.toLowerCase(x);
-        y = Character.toLowerCase(y);
+        if (x >= 'A' && x <= 'Z') {
+            x = Character.toLowerCase(x);
+        }
+        if (y >= 'A' && y <= 'Z') {
+            y = Character.toLowerCase(y);
+        }
         return Math.abs(x - y) == 1;
     }
 }
