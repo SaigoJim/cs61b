a53c89d2579f5c9396a17fd1efcc48cf606f3381
SaigoJim <1533137229@qq.com>
2021-06-03 12:14:53 +0800

    improve percolation to higher and more beautiful level

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,199 +2,123 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import java.util.ArrayList;
-
 public class Percolation {
-    private Site[][] grid;
-    private int length;
+    private int LENGTH;
     private int openSize;
-    WeightedQuickUnionUF wqu;
-    boolean percolated;
-    ArrayList<Integer> openedTopSites;
-    private class Site {
-        final boolean OPEN = true;
-        final boolean BLOCKED = false;
-        boolean fulled;
-        boolean state;
-        int lengthOfGrid;
-        int row;
-        int col;
-        int pos;
-        boolean isTop = false;
-        boolean isBottem = false;
-        Site(int length, int row, int col) {
-            lengthOfGrid = length;
-            this.row = row;
-            this.col = col;
-            fulled = false;
-            setBLOCKED();
-            pos = TwoDToOneD(row, col);
-            if (row == 0) {
-                isTop = true;
-            } else if (row == length - 1) {
-                isBottem = true;
-            }
-        }
+    boolean OPENED = true;
+    boolean BLOCKED = false;
+    Site[][] grid;
+    int topSites = LENGTH * LENGTH;
+    int bottomSites = LENGTH * LENGTH + 1;
+    WeightedQuickUnionUF percolationWQU;
+    WeightedQuickUnionUF fullWQU;
 
-        private int TwoDToOneD(int row, int col) {
-            return length*row + col;
-        }
-        public void setBLOCKED() {
+    private class Site {
+        boolean state;
+        int pos;
+        Site(int pos) {
+            this.pos = pos;
             state = BLOCKED;
         }
-        public void setOPEN() {
-            state = OPEN;
-        }
-        public void setFulled() {
-            fulled = true;
-        }
-        public void setFulled(WeightedQuickUnionUF wqu, int top) {
-            if (wqu.connected(pos, top)) {
-                fulled = true;
-            }
-        }
-        public boolean peekState() {
-            return state;
-        }
-        public boolean isFulled() {
-            return fulled;
+
+        public void setOPENED() {
+            state = OPENED;
         }
     }
-
-    // create N-by-N grid, with all sites initially blocked
+    /** create N-by-N grid, with all sites initially blocked */
     public Percolation(int N){
         if (N <= 0) {
             throw new java.lang.IllegalArgumentException();
         }
-        grid = new Site[N][N];
-        length = N;
+        LENGTH = N;
+        openSize = 0;
+        grid = new Site[LENGTH][LENGTH];
         for (int i = 0; i < N; i += 1) {
             for (int j = 0; j < N; j += 1) {
-                grid[i][j] = new Site(length, i, j);
+                int pos = TwoToOne(i, j);
+                grid[i][j] = new Site(pos);
             }
         }
-        wqu = new WeightedQuickUnionUF(N*N);
-        openSize = 0;
-        percolated = false;
-        openedTopSites = new ArrayList<>();
+        percolationWQU = new WeightedQuickUnionUF(LENGTH*LENGTH + 2);
+        fullWQU = new WeightedQuickUnionUF(LENGTH*LENGTH + 1);
+    }
+    private int TwoToOne(int row, int col) {
+        return row * LENGTH + col;
     }
 
-    // open the site (row, col) if it is not open already
+    /** open the site (row, col) if it is not open already */
     public void open(int row, int col) {
-        if (row >= length || col >= length) {
+        if (row < 0 || row >= LENGTH || col < 0 || col >= LENGTH) {
             throw new java.lang.IndexOutOfBoundsException();
         }
         if (isOpen(row, col)) {
             return;
         }
-        grid[row][col].setOPEN();
-        openSize += 1;
-        unionNeighbors(grid[row][col]);
-        updateStates(grid[row][col]);
+        grid[row][col].setOPENED();
+        unionNeighbors(row, col);
     }
-    private void updateStates(Site s) {
-        if (s.isTop) {
-            if (!alreadyConnectedToTop(s.pos)) {
-                openedTopSites.add(s.pos);
-            }
-        } else if (s.isBottem) {
-            if (alreadyConnectedToTop(s.pos)) {
-                s.setFulled();
-                percolated = true;
-            }
-        } else {
-            if (alreadyConnectedToTop(s.pos)) {
-                s.setFulled();
-            }
+    private void unionNeighbors(int row, int col) {
+        if (row == 0) {
+            percolationWQU.union(grid[row][col].pos, topSites);
+            fullWQU.union(grid[row][col].pos, topSites);
+        } else if (row == LENGTH - 1) {
+            percolationWQU.union(grid[row][col].pos, bottomSites);
         }
-    }
 
-    private boolean alreadyConnectedToTop(int pos) {
-        boolean alreadyConnected = false;
-        for (int topPos : openedTopSites) {
-            if (wqu.connected(topPos, pos)) {
-                alreadyConnected = true;
-            }
-        }
-        return alreadyConnected;
-    }
-    private void unionNeighbors(Site s) {
-        Site neighbor;
+        // Neighbors
         // UP
-        if (s.row - 1 > 0) {
-            neighbor = grid[s.row - 1][s.col];
-            wqu.union(neighbor.pos, s.pos);
+        if (row - 1 > - 1) {
+            if (isOpen(row - 1, col)) {
+                percolationWQU.union(grid[row][col].pos, grid[row-1][col].pos);
+                fullWQU.union(grid[row][col].pos, grid[row-1][col].pos);
+            }
         }
         // DOWN
-        if (s.row + 1 < length) {
-            neighbor = grid[s.row + 1][s.col];
-            wqu.union(neighbor.pos, s.pos);
+        if (row + 1 < LENGTH) {
+            if (isOpen(row + 1, col)) {
+                percolationWQU.union(grid[row][col].pos, grid[row+1][col].pos);
+                fullWQU.union(grid[row][col].pos, grid[row+1][col].pos);
+            }
         }
         // LEFT
-        if (s.col - 1 > 0) {
-            neighbor = grid[s.row][s.col - 1];
-            wqu.union(neighbor.pos, s.pos);
+        if (col - 1 > - 1) {
+            if (isOpen( row, col - 1)) {
+                percolationWQU.union(grid[row][col].pos, grid[row][col-1].pos);
+                fullWQU.union(grid[row][col].pos, grid[row][col-1].pos);
+            }
         }
-        // RIGHT
-        if (s.col + 1 < length) {
-            neighbor = grid[s.row][s.col + 1];
-            wqu.union(neighbor.pos, s.pos);
+        // DOWN
+        if (col + 1 < LENGTH) {
+            if (isOpen(row, col + 1)) {
+                percolationWQU.union(grid[row][col].pos, grid[row][col+1].pos);
+                fullWQU.union(grid[row][col].pos, grid[row][col+1].pos);
+            }
         }
     }
-
-    private Site findOpenedNeighbors(int row, int col) {
-        try {
-            // UP
-            if (isOpen(row - 1, col)) {return grid[row - 1][col];}
-        } catch (Exception e) {
-        }
-        try {
-            // DOWN
-            if (isOpen(row + 1, col)) {return grid[row + 1][col];}
-        } catch (Exception e) {
-        }
-        try {
-            // LEFT
-            if (isOpen(row, col - 1)) {return grid[row][col - 1];}
-        } catch (Exception e) {
-        }
-        try {
-            // RIGHT
-            if (isOpen(row, col + 1)) {return grid[row][col + 1];}
-        } catch (Exception e) {
-        }
-        return null;
-    }
-
-    // is the site (row, col) open?
+    /** is the site (row, col) open? */
     public boolean isOpen(int row, int col) {
-        if (row < 0 || row >= length || col < 0 || col >= length) {
+        if (row < 0 || row >= LENGTH || col < 0 || col >= LENGTH) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        return grid[row][col].peekState();
+        return grid[row][col].state;
     }
 
-    // is the site (row, col) full?
+    /** is the site (row, col) full? */
     public boolean isFull(int row, int col) {
-        if (row < 0 || row >= length || col < 0 || col >= length) {
+        if (row < 0 || row >= LENGTH || col < 0 || col >= LENGTH) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        return grid[row][col].isFulled();
+        return fullWQU.connected(grid[row][col].pos, topSites);
     }
 
-    // number of open sites
+    /** number of open sites */
     public int numberOfOpenSites() {
         return openSize;
     }
 
-    // does the system percolate?
+    /** does the system percolate? */
     public boolean percolates() {
-        if (openSize < 1) {
-            return false;
-        } else if (openSize == length*length) {
-            return true;
-        }
-        return percolated;
+        return percolationWQU.connected(topSites, bottomSites);
     }
 
     // use for unit testing (not required)
---
+++ hw2/hw2/TestPercolation.java
@@ -0,0 +1,51 @@
+package hw2;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestPercolation {
+    @Test
+    public void testPercolation() {
+        Percolation p = new Percolation(5);
+        p.open(0, 0);
+        p.open(0, 2);
+        p.open(0, 3);
+        p.open(1, 2);
+        p.open(2, 1);
+        p.open(2, 2);
+        p.open(3, 1);
+
+        int size = p.numberOfOpenSites();
+        boolean percolated = p.percolates();
+        assertFalse(percolated);
+
+        boolean isFulled = p.isFull(1, 2);
+        assertTrue(isFulled);
+    }
+
+    @Test
+    public void testPercolationBackWash() {
+        Percolation p = new Percolation(5);
+        p.open(0, 0);
+
+        p.open(0, 2);
+        p.open(1, 2);
+        p.open(2, 2);
+        p.open(3, 2);
+        p.open(4, 2);
+
+        p.open(3, 4);
+        p.open(4, 4);
+
+
+        int size = p.numberOfOpenSites();
+        boolean percolated = p.percolates();
+        assertTrue(percolated);
+
+        boolean isFulled1 = p.isFull(3, 4);
+        assertFalse(isFulled1);
+
+        boolean isFulled2 = p.isFull(2, 2);
+        assertTrue(isFulled2);
+
+    }
+}
