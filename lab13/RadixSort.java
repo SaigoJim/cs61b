d8a82ed0c3b0404af60c057b19648be392285d4f
SaigoJim <1533137229@qq.com>
2021-07-25 00:57:55 +0800

    modified lab13 LSD and MSD

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -22,12 +22,11 @@ public class RadixSort {
         }
         String[] strs = new String[asciis.length];
         System.arraycopy(asciis, 0, strs, 0, strs.length);
-        for (int i = 0; i < maxWidth; i += 1) {
+        for (int i = maxWidth - 1; i > - 1; i -= 1) {
             sortHelperLSD(strs, i);
         }
         return strs;
     }
-
     /**
      * LSD helper method that performs a destructive counting sort the array of
      * Strings based off characters at a specific index.
@@ -64,13 +63,32 @@ public class RadixSort {
 
     private static int placeOfStringAtIndex(String ascii, int index) {
         if (index > ascii.length() - 1) {
-            return ascii.charAt(0);
+            return 0;
         } else {
-            int leastSignificantIndex = ascii.length() - index - 1;
-            char radixChar = ascii.charAt(leastSignificantIndex);
+            char radixChar = ascii.charAt(index);
             int pos = (int) radixChar;
             return pos;
         }
+    }
+    /**
+     * Does MSD radix sort on the passed in array with the following restrictions:
+     * The array can only have ASCII Strings (sequence of 1 byte characters)
+     * The sorting is stable and non-destructive
+     * The Strings can be variable length (all Strings are not constrained to 1 length)
+     *
+     * @param asciis String[] that needs to be sorted
+     *
+     * @return String[] the sorted array
+     */
+    public static String[] msd(String[] asciis) {
+        int maxWidth = Integer.MIN_VALUE;
+        for (String ascii : asciis) {
+            maxWidth = maxWidth < ascii.length() ? ascii.length() : maxWidth;
+        }
+        String[] strs = new String[asciis.length];
+        System.arraycopy(asciis, 0, strs, 0, strs.length);
+        sortHelperMSD(strs, 0, strs.length, 0, maxWidth);
+        return strs;
     }
     /**
      * MSD radix sort helper function that recursively calls itself to achieve the sorted array.
@@ -82,8 +100,50 @@ public class RadixSort {
      * @param index the index of the character the method is currently sorting on
      *
      **/
-    private static void sortHelperMSD(String[] asciis, int start, int end, int index) {
+    private static void sortHelperMSD(String[] asciis, int start, int end, int index, int maxWidth) {
+        if (asciis.length < 1 || end -start == 1 || index >= maxWidth) {
+            return;
+        }
         // Optional MSD helper method for optional MSD radix sort
-        return;
+        // gather all the counts for each value
+        int[] counts = new int[256];
+        for (int i = start; i < end; i += 1) {
+            String ascii = asciis[i];
+            int pos = placeOfStringAtIndex(ascii, index);
+            counts[pos] += 1;
+        }
+        // A generalized implementation of
+        // counting sort that uses start position calculation
+        int currNum = 0;
+        for (int i = 0; i < counts.length; i += 1) {
+            int temp = counts[i];
+            counts[i] = currNum;
+            currNum += temp;
+        }
+        String[] strs = new String[asciis.length];
+        System.arraycopy(asciis, 0, strs, 0, strs.length);
+        for (int i = start; i < end; i += 1) {
+            String item = strs[i];
+            int asciiValue = placeOfStringAtIndex(item, index);
+            // Increment
+            int place = counts[asciiValue] + start;
+            asciis[place] = item;
+            counts[asciiValue] += 1;
+        }
+        for (int nextEnd = start; nextEnd <= end; nextEnd += 1) {
+            if (nextEnd == end) {
+                sortHelperMSD(asciis, start, nextEnd, index + 1, maxWidth);
+                start = nextEnd;
+            }
+            if (nextEnd == end) {
+                break;
+            }
+            int startPos = placeOfStringAtIndex(asciis[start], index);
+            int nextEndPos = placeOfStringAtIndex(asciis[nextEnd], index);
+            if (startPos != nextEndPos) {
+                sortHelperMSD(asciis, start, nextEnd, index + 1, maxWidth);
+                start = nextEnd;
+            }
+        }
     }
 }
--- lab13/RadixSortTester.java
+++ lab13/RadixSortTester.java
@@ -11,4 +11,47 @@ public class RadixSortTester {
         Arrays.sort(asciis);
         assertArrayEquals(asciis, sorted);
     }
+    @Test
+    public void testLSD1() {
+        String[] asciis = new String[]{"56", "112", "94", "4", "9", "82", "394", "80"};
+        String[] sorted = RadixSort.sort(asciis);
+        Arrays.sort(asciis);
+        assertArrayEquals(asciis, sorted);
+    }
+    @Test
+    public void testLSD2() {
+        String[] asciis = new String[]{"94", "9"};
+        String[] sorted = RadixSort.sort(asciis);
+        Arrays.sort(asciis);
+        assertArrayEquals(asciis, sorted);
+    }
+    @Test
+    public void testStringCompareTo() {
+        int r = "zzz".compareTo("zsdf");
+        String a = "zzz";
+        String b = "zsdf";
+        int d = a.compareTo(b);
+        assertTrue(r > 0);
+    }
+    @Test
+    public void testMSD() {
+        String[] asciis = new String[]{"abcd", "zsdf", "zzz", "asdggh"};
+        String[] sorted = RadixSort.msd(asciis);
+        Arrays.sort(asciis);
+        assertArrayEquals(asciis, sorted);
+    }
+    @Test
+    public void testMSD1() {
+        String[] asciis = new String[]{"56", "112", "94", "4", "9", "82", "394", "80"};
+        String[] sorted = RadixSort.msd(asciis);
+        Arrays.sort(asciis);
+        assertArrayEquals(asciis, sorted);
+    }
+    @Test
+    public void testMSD2() {
+        String[] asciis = new String[]{"94", "9"};
+        String[] sorted = RadixSort.msd(asciis);
+        Arrays.sort(asciis);
+        assertArrayEquals(asciis, sorted);
+    }
 }
